ded9cbc1f5b3a6207ec7893c14141fce4a2b1d3b
LAUNAY Samuel <107540223+Lainow@users.noreply.github.com>
2023-03-17 21:58:10 +0100

    COrrectionBugLancementApp

--- app/src/main/java/com/example/projetandroidss/HomeActivity.kt
+++ app/src/main/java/com/example/projetandroidss/HomeActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,56 +32,66 @@ class HomeActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
+
                 ) {
                     InsertionDonnees(application = application)
                     val list: List<Formation>? = FormationViewModel(application).getAll()
                     val initiateur = InitiatorViewModel(application).getById(1)
 
-                    Column(modifier = Modifier.fillMaxWidth()) {
-                        Row (modifier = Modifier.fillMaxWidth()){
-                            Button(modifier = Modifier.fillMaxWidth(), onClick = {
-                                val intent = Intent(this@HomeActivity, ProfileActivity::class.java)
-                                if (initiateur != null) {
-                                    intent.putExtra("idInitiateur", initiateur.id)
-                                    startActivity(intent)
-                                }
-                            }) {
-                                Text("Profil")
-                            }
-                        }
-                        Text(text = "Liste des Eleves",
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp,
-                            color = Color.White,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(Color(0xFF2F1DAF))
-                                .padding(16.dp)
-                        )
-                        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                            Column() {
-                                list?.forEach { data ->
-                                    Text(text = data.name)
+                    Box(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            Row (modifier = Modifier.fillMaxWidth()) {
+                                Button(modifier = Modifier.fillMaxWidth().background(Color.White), onClick = {
+                                    val intent = Intent(this@HomeActivity, ProfileActivity::class.java)
+                                    if (initiateur != null) {
+                                        intent.putExtra("idInitiateur", initiateur.id)
+                                        startActivity(intent)
+                                    }
+                                }) {
+                                    Text("Mon Profil")
                                 }
                             }
-                        }
-                        Text(text = "Liste des Eleves",
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp,
-                            color = Color.White,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(Color(0xFF2F1DAF))
-                                .padding(16.dp)
-                        )
-                        var listStu: List<Student>? = StudentViewModel(application).getAll()
-                        Row(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
-                            Column() {
+                            Row (modifier = Modifier.fillMaxWidth().background(Color(0xFF2F1DAF))
+                                .padding(16.dp)) {
+                                Text(
+                                    text = "Liste des Formations",
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 20.sp,
+                                    color = Color.White,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                )
+                            }
+                            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                                Column() {
+                                    list?.forEach { data ->
+                                        Text(text = data.name)
+                                    }
+                                }
+                            }
+                            Text(text = "Liste des Eleves",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color(0xFF2F1DAF))
+                                    .padding(16.dp)
+                            )
+                            var listStu: List<Student>? = StudentViewModel(application).getAll()
+                            Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
                                 listStu?.forEach { data ->
                                     Text(text = data.name)
+                                    Button(modifier = Modifier.fillMaxWidth().background(Color.White), onClick = {
+                                        val intent = Intent(this@HomeActivity, StudentInfoActivity::class.java)
+                                        intent.putExtra("idStudent", data.id)
+                                        startActivity(intent)
+                                    }) {
+                                        Text("Details")
+                                    }
                                 }
                             }
                         }

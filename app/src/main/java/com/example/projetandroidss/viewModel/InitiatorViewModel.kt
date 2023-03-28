8cdb62f03c9f4bdb1187c1424856af1fa163a825
LAUNAY Samuel <107540223+Lainow@users.noreply.github.com>
2023-03-28 18:42:03 +0200

    ProjetFinal

--- app/src/main/java/com/example/projetandroidss/viewModel/InitiatorViewModel.kt
+++ app/src/main/java/com/example/projetandroidss/viewModel/InitiatorViewModel.kt
@@ -11,6 +11,9 @@ import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 class InitiatorViewModel(app: Application) : AndroidViewModel(app) {
 
